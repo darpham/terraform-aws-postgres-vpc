16679880ef29238306da3dd949d62f8781a8af8e
Darren Pham <phamdarren1@gmail.com>
2020-12-09 15:24:51 -0800

    creating bastion module that imports github public ssh keys

--- bastion-gh/main.tf
+++ bastion-gh/main.tf
@@ -8,53 +8,6 @@ locals {
   subnet_id = element(var.public_subnet_ids, 1)
 }
 
-resource "aws_security_group" "bastion" {
-  name        = var.name
-  vpc_id      = var.vpc_id
-  description = "Bastion security group (only SSH inbound access is allowed)"
-
-  tags = {
-    Name = var.name
-  }
-}
-
-resource "aws_security_group_rule" "ssh_ingress" {
-  type              = "ingress"
-  from_port         = "22"
-  to_port           = "22"
-  protocol          = "tcp"
-  cidr_blocks       = var.allowed_cidr
-  ipv6_cidr_blocks  = var.allowed_ipv6_cidr
-  security_group_id = aws_security_group.bastion.id
-}
-
-resource "aws_security_group_rule" "ssh_sg_ingress" {
-  count                    = length(var.allowed_security_groups)
-  type                     = "ingress"
-  from_port                = "22"
-  to_port                  = "22"
-  protocol                 = "tcp"
-  source_security_group_id = element(var.allowed_security_groups, count.index)
-  security_group_id        = aws_security_group.bastion.id
-}
-
-resource "aws_security_group_rule" "bastion_all_egress" {
-  type      = "egress"
-  from_port = "0"
-  to_port   = "65535"
-  protocol  = "all"
-
-  cidr_blocks = [
-    "0.0.0.0/0",
-  ]
-
-  ipv6_cidr_blocks = [
-    "::/0",
-  ]
-
-  security_group_id = aws_security_group.bastion.id
-}
-
 data "template_file" "user_data" {
   template = file("${path.module}/${var.user_data_file}")
 
